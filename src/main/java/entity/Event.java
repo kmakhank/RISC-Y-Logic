3c56a274f0f1cb2e0767f885233e407bf5195cd2
kmakhank <52182338+saparadox@users.noreply.github.com>
2024-11-04 18:12:48 -0500

    Completed requisite clean architecture packaging structure

--- src/main/java/entity/Event.java
+++ src/main/java/entity/Event.java
@@ -13,4 +13,12 @@ public class Event {
     private final Set<String> tags;
     private final String source;
 
+    public Event(UUID id, String title, String description, LocalDateTime startTime, Set<String> tags, String source) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.startTime = startTime;
+        this.tags = tags;
+        this.source = source;
+    }
 }
